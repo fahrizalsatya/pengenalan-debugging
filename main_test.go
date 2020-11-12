346ed8a1fe7a30c1d3bf6fae1815f6966237b979
fahrizalsatya <fahrizalsatya.laksamana@gmail.com>
2020-11-12 19:14:14 +0700

    Change file main_test.go

--- main_test.go
+++ main_test.go
@@ -3,22 +3,9 @@ package main
 import "testing"
 
 func Test_add(t *testing.T) {
-	type args struct {
-		a int
-		b int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := add(tt.args.a, tt.args.b); got != tt.want {
-				t.Errorf("add() = %v, want %v", got, tt.want)
-			}
-		})
+	a, b, c := 1, 2, 3
+	res := add(a, b)
+	if res != c {
+		t.Fail()
 	}
 }
