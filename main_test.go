a63c1e5490393a3577f26ba89d514afe64c4f7f8
fahrizalsatya <fahrizalsatya.laksamana@gmail.com>
2020-11-12 19:08:41 +0700

    Change file main_test.go

--- main_test.go
+++ main_test.go
@@ -2,33 +2,6 @@ package main
 
 import "testing"
 
-func TestAvenger_isAlive(t *testing.T) {
-	tests := []struct {
-		name string
-		a    *Avenger
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.a.isAlive()
-		})
-	}
-}
-
-func Test_main(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			main()
-		})
-	}
-}
-
 func Test_add(t *testing.T) {
 	type args struct {
 		a int
