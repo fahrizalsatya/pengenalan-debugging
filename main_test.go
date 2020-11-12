96fc7faba9fc8d2f1bfa2f0cad2302ed402de1ab
fahrizalsatya <fahrizalsatya.laksamana@gmail.com>
2020-11-12 19:07:01 +0700

    Add file main_test.go

---
+++ main_test.go
@@ -0,0 +1,51 @@
+package main
+
+import "testing"
+
+func TestAvenger_isAlive(t *testing.T) {
+	tests := []struct {
+		name string
+		a    *Avenger
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.a.isAlive()
+		})
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
+
+func Test_add(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
